syberia-project/platform_packages_apps_SyberiaSettings

Add vibration patterns from OOS [3/3]
Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: I6395786acfc493f41eca30e84c8b5ef8b53eb9f1

src/com/syberia/settings/fragments/DialerSettings.java
@@ -19,6 +19,9 @@ package com.syberia.settings.fragments;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.media.AudioAttributes;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -40,24 +43,187 @@ import com.syberia.settings.Utils;
 import com.android.internal.logging.nano.MetricsProto;
 
 import com.syberia.settings.preference.CustomSeekBarPreference;
+import com.android.settings.widget.RadioButtonPreference;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class DialerSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener, Indexable {
+public class DialerSettings extends SettingsPreferenceFragment implements
+                 OnPreferenceChangeListener, Preference.OnPreferenceClickListener, Indexable {
 
     private static final String FLASH_ON_CALL_WAITING_DELAY = "flash_on_call_waiting_delay";
     private CustomSeekBarPreference mFlashOnCallWaitingDelay;
+    private Context mContext;
+    private Vibrator mVibrator;
+
+    private final AudioAttributes mAudioAttributesRingtone = new AudioAttributes.Builder()
+        .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+        .build();
+
+    private static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
+    private static final String[] mKeys = {"pattern_dzzz_dzzz", "pattern_dzzz_da", "pattern_mm_mm_mm",
+        "pattern_da_da_dzzz", "pattern_da_dzzz_da"};
+
+    private final Map<String, RadioButtonPreference> mStringToPreferenceMap = new HashMap<>();
+
+    private RadioButtonPreference[] mRadioPreferences = new RadioButtonPreference[5];
+
+    private static final long[] DZZZ_DZZZ_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        800, // How long to vibrate
+        800, // How long to wait before vibrating again
+        800, // How long to vibrate
+        800, // How long to wait before vibrating again
+    };
+
+    private static final long[] DZZZ_DA_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        500, // How long to vibrate
+        200, // Delay
+        20, // How long to vibrate
+        720, // How long to wait before vibrating again
+    };
+
+    private static final long[] MM_MM_MM_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        300, // How long to vibrate
+        400, // Delay
+        300, // How long to vibrate
+        400, // Delay
+        300, // How long to vibrate
+        1700, // How long to wait before vibrating again
+    };
+
+    private static final long[] DA_DA_DZZZ_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        30, // How long to vibrate
+        80, // Delay
+        30, // How long to vibrate
+        80, // Delay
+        50,  // How long to vibrate
+        180, // Delay
+        600,  // How long to vibrate
+        1050, // How long to wait before vibrating again
+    };
+
+    private static final long[] DA_DZZZ_DA_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        80, // How long to vibrate
+        200, // Delay
+        600, // How long to vibrate
+        150, // Delay
+        20,  // How long to vibrate
+        1050, // How long to wait before vibrating again
+    };
+
+    private static final int[] NINE_ELEMENTS_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
+        255,
+        0,
+        255,
+        0,
+        255,
+        0,
+    };
+
+    private static final int[] SEVEN_ELEMENTS_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
+        255,
+        0,
+        255,
+        0,
+    };
+
+    private static final int[] FIVE_ELEMENTS_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
+        255,
+        0,
+    };
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+
+        mContext = getActivity().getApplicationContext();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        if (mVibrator != null && !mVibrator.hasVibrator()) {
+            mVibrator = null;
+        }
+
         addPreferencesFromResource(R.xml.dialer_settings);
         final ContentResolver resolver = getActivity().getContentResolver();
 
         mFlashOnCallWaitingDelay = (CustomSeekBarPreference) findPreference(FLASH_ON_CALL_WAITING_DELAY);
         mFlashOnCallWaitingDelay.setValue(Settings.System.getInt(resolver, Settings.System.FLASH_ON_CALLWAITING_DELAY, 200));
         mFlashOnCallWaitingDelay.setOnPreferenceChangeListener(this);
+
+        for (int i = 0; i < 5; i++) {
+            mRadioPreferences[i] = (RadioButtonPreference) findPreference(mKeys[i]);
+            mStringToPreferenceMap.put(mKeys[i], mRadioPreferences[i]);
+            mRadioPreferences[i].setOnPreferenceClickListener(this);
+        }
+
+        final int currentPattern = Settings.System.getIntForUser(resolver, RINGTONE_VIBRATION_PATTERN, 0, UserHandle.USER_CURRENT);
+
+        updateVibrationPattern(currentPattern);
+    }
+
+    private void updateVibrationPattern(int val) {
+        for (int i = 0; i < 5; i++) {
+            ((RadioButtonPreference) mStringToPreferenceMap.get(mKeys[i])).setChecked((val == i) ? true:false);
+        }
+        Settings.System.putIntForUser(getContentResolver(), RINGTONE_VIBRATION_PATTERN,
+                val, UserHandle.USER_CURRENT);
+    }
+
+    private void performVibrationDemo(int val) {
+        VibrationEffect mDefaultVibrationEffect;
+        switch(val) {
+            case 1:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DZZZ_DA_VIBRATION_PATTERN,
+                    FIVE_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            case 2:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(MM_MM_MM_VIBRATION_PATTERN,
+                    SEVEN_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            case 3:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DA_DA_DZZZ_VIBRATION_PATTERN,
+                    NINE_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            case 4:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DA_DZZZ_DA_VIBRATION_PATTERN,
+                    SEVEN_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            default:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DZZZ_DZZZ_VIBRATION_PATTERN,
+                    FIVE_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+        }
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            mVibrator.vibrate(mDefaultVibrationEffect, mAudioAttributesRingtone);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        final String key = preference.getKey();
+        if (preference instanceof RadioButtonPreference) {
+            int val = Arrays.asList(mKeys).indexOf(key);
+            updateVibrationPattern(val);
+            performVibrationDemo(val);
+        }
+        return true;
     }
 
     @Override

aryaman895/packages_apps_CafexSettings

src/com/cafex/settings/fragments/DialerSettings.java
@@ -20,6 +20,9 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.media.AudioAttributes;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import androidx.preference.Preference;
@@ -34,6 +37,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.widget.RadioButtonPreference;
 
 import com.cafex.settings.Utils;
 
@@ -42,22 +46,183 @@ import com.android.internal.logging.nano.MetricsProto;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SearchIndexable
-public class DialerSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class DialerSettings extends SettingsPreferenceFragment implements
+                 OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
+    private Context mContext;
+    private Vibrator mVibrator;
+
+    private final AudioAttributes mAudioAttributesRingtone = new AudioAttributes.Builder()
+        .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+        .build();
+
+    private static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
+    private static final String[] mKeys = {"pattern_dzzz_dzzz", "pattern_dzzz_da", "pattern_mm_mm_mm",
+        "pattern_da_da_dzzz", "pattern_da_dzzz_da"};
+
+    private final Map<String, RadioButtonPreference> mStringToPreferenceMap = new HashMap<>();
+
+    private RadioButtonPreference[] mRadioPreferences = new RadioButtonPreference[5];
+
+    private static final long[] DZZZ_DZZZ_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        800, // How long to vibrate
+        800, // How long to wait before vibrating again
+        800, // How long to vibrate
+        800, // How long to wait before vibrating again
+    };
+
+    private static final long[] DZZZ_DA_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        500, // How long to vibrate
+        200, // Delay
+        20, // How long to vibrate
+        720, // How long to wait before vibrating again
+    };
+
+    private static final long[] MM_MM_MM_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        300, // How long to vibrate
+        400, // Delay
+        300, // How long to vibrate
+        400, // Delay
+        300, // How long to vibrate
+        1700, // How long to wait before vibrating again
+    };
+
+    private static final long[] DA_DA_DZZZ_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        30, // How long to vibrate
+        80, // Delay
+        30, // How long to vibrate
+        80, // Delay
+        50,  // How long to vibrate
+        180, // Delay
+        600,  // How long to vibrate
+        1050, // How long to wait before vibrating again
+    };
+
+    private static final long[] DA_DZZZ_DA_VIBRATION_PATTERN = {
+        0, // No delay before starting
+        80, // How long to vibrate
+        200, // Delay
+        600, // How long to vibrate
+        150, // Delay
+        20,  // How long to vibrate
+        1050, // How long to wait before vibrating again
+    };
+
+    private static final int[] NINE_ELEMENTS_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
+        255,
+        0,
+        255,
+        0,
+        255,
+        0,
+    };
+
+    private static final int[] SEVEN_ELEMENTS_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
+        255,
+        0,
+        255,
+        0,
+    };
+
+    private static final int[] FIVE_ELEMENTS_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
+        255,
+        0,
+    };
     
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+
+        mContext = getActivity().getApplicationContext();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        if (mVibrator != null && !mVibrator.hasVibrator()) {
+            mVibrator = null;
+        }        
         addPreferencesFromResource(R.xml.dialer_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
 
         PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
         if (!Utils.isVoiceCapable(getActivity())) {
                 prefScreen.removePreference(incallVibCategory);
-        }                
+        }
+
+        for (int i = 0; i < 5; i++) {
+            mRadioPreferences[i] = (RadioButtonPreference) findPreference(mKeys[i]);
+            mStringToPreferenceMap.put(mKeys[i], mRadioPreferences[i]);
+            mRadioPreferences[i].setOnPreferenceClickListener(this);
+        }
+
+        final int currentPattern = Settings.System.getIntForUser(resolver, RINGTONE_VIBRATION_PATTERN, 0, UserHandle.USER_CURRENT);
+
+        updateVibrationPattern(currentPattern);
+    }
+
+    private void updateVibrationPattern(int val) {
+        for (int i = 0; i < 5; i++) {
+            ((RadioButtonPreference) mStringToPreferenceMap.get(mKeys[i])).setChecked((val == i) ? true:false);
+        }
+        Settings.System.putIntForUser(getContentResolver(), RINGTONE_VIBRATION_PATTERN,
+                val, UserHandle.USER_CURRENT);
+    }
+
+    private void performVibrationDemo(int val) {
+        VibrationEffect mDefaultVibrationEffect;
+        switch(val) {
+            case 1:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DZZZ_DA_VIBRATION_PATTERN,
+                    FIVE_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            case 2:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(MM_MM_MM_VIBRATION_PATTERN,
+                    SEVEN_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            case 3:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DA_DA_DZZZ_VIBRATION_PATTERN,
+                    NINE_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            case 4:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DA_DZZZ_DA_VIBRATION_PATTERN,
+                    SEVEN_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+            default:
+                mDefaultVibrationEffect = VibrationEffect.createWaveform(DZZZ_DZZZ_VIBRATION_PATTERN,
+                    FIVE_ELEMENTS_VIBRATION_AMPLITUDE, -1);
+                break;
+        }
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            mVibrator.vibrate(mDefaultVibrationEffect, mAudioAttributesRingtone);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        final String key = preference.getKey();
+        if (preference instanceof RadioButtonPreference) {
+            int val = Arrays.asList(mKeys).indexOf(key);
+            updateVibrationPattern(val);
+            performVibrationDemo(val);
+        }
+        return true;                    
     }
 
     @Override